30acafdc66330f13c4f8f8bd81c980301dffdddc
ArthurSar <74473536+ArthurSar@users.noreply.github.com>
2021-01-15 15:40:07 +0300

    Add files via upload

---
+++ Стильтекс/script.js
@@ -0,0 +1,16 @@
+    $(document).ready (function (){
+      $('[data-toggle="tooltip"]').tooltip ();
+    });
+
+    $(document).ready(function(){ 
+      var body = $("body"); 
+      body.fadeIn(400); 
+      $(document).on("click", "a:not([href^='#']):not([href^='tel']):not([href^='mailto'])", function(e) { 
+      e.preventDefault(); 
+      $("body").fadeOut(400); 
+      var self = this;
+      setTimeout(function () { 
+        window.location.href = $(self).attr("href"); 
+      }, 400); 
+      });
+    }); 
